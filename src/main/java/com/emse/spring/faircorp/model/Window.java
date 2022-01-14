4ae7b72b1b9ed846c9dec16ccba7e7de7da477bd
KalomeB <90754244+KalomeB@users.noreply.github.com>
2022-01-14 15:09:42 +0100

    Add javadoc

--- src/main/java/com/emse/spring/faircorp/model/Window.java
+++ src/main/java/com/emse/spring/faircorp/model/Window.java
@@ -2,6 +2,9 @@ package com.emse.spring.faircorp.model;
 
 import javax.persistence.*;
 
+/** Represents a window
+*/
+
 @Entity // (1)
 @Table(name = "RWINDOW")// (2)
 public class Window {
@@ -23,40 +26,69 @@ public class Window {
     public Window() {
     }
 
+    /** Creates a window with the specified name, status and room
+    * @param name       The window's name
+    * @param status     The window's status
+    * @param room       The window's room
+    */
     public Window(String name, WindowStatus status, Room room) {
         this.windowStatus = status;
         this.name = name;
         this.room = room;
     }
 
+    /** Gets the window's ID
+    * @return       a Long representing the window's ID
+    */
     public Long getId() {
         return this.id;
     }
 
+    /** Sets the window's ID
+    * @param id     a Long containing the window's ID
+    */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /** Gets the window's room
+    * @return       a Room representing the window's room
+    */
     public Room getRoom() {
         return room;
     }
 
+    /** Sets the window's room
+    * @param room       a Room containing the window's room
+    */
     public void setRoom(Room room) {
         this.room = room;
     }
 
+    /** Gets the window's name
+    * @return       a String representing the window's name
+    */
     public String getName() {
         return name;
     }
 
+    /** Sets the window's name
+    * @param name       a String containing the window's name
+    */
     public void setName(String name) {
         this.name = name;
     }
 
+    /** Gets the window's status
+    * @return       a WindowStatus representing the window's status
+    */
     public WindowStatus getWindowStatus() {
         return windowStatus;
     }
 
+    /** Sets the window's status
+    * @param windowStatus       a WindowStatus containing the window's status
+    */
     public void setWindowStatus(WindowStatus windowStatus) {
         this.windowStatus = windowStatus;
     }
